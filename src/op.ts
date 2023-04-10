b58d44b546cf4ea81a274ed88c6380cd3a75cc98
Jerry Wang <x.jerry.wang@gmail.com>
2023-04-10 17:41:40 +0800

    test render fragment / list / if

--- src/op.ts
+++ src/op.ts
@@ -1,3 +1,21 @@
-export function unmount() {
+import { isString } from '@0x-jerry/utils'
+import { DComponent, isFragment, getContext } from './node'
 
+export function mount(dom: DComponent, selector: string | HTMLElement) {
+  const container = isString(selector)
+    ? document.querySelector(selector)
+    : selector
+
+  if (!container) {
+    console.warn(`Can't find container`)
+    return
+  }
+
+  if (isFragment(dom)) {
+    dom.moveTo(container)
+  } else {
+    container.append(dom)
+  }
+
+  getContext(dom).emit('mounted')
 }
--- src/test.tsx
+++ src/test.tsx
@@ -1,32 +1,15 @@
-import { computed, ref } from '@vue/reactivity'
+import { computed, ref, toRaw } from '@vue/reactivity'
 import { h, Fragment } from './jsx'
-import { DComponent, getContext, isFragment } from './node'
 import { vCase, vMap } from './internalComponents'
+import { mount } from './op'
 
-const Counter = () => {
-  const count = ref(0)
+const TestFor = () => {
+  let idx = 0
 
-  const show = ref(false)
-
-  const click = () => {
-    count.value++
-    console.log('click', count.value)
-  }
-
-  const testVCase = vCase(show, {
-    true: () => <div>this is true</div>,
-    false: () => <div>this is false</div>,
-  })
-
-  const toggleShow = () => {
-    show.value = !show.value
-  }
-
-  const list = ref(
-    new Array(10).fill(0).map((_, idx) => ({ key: idx.toString() })),
-  )
+  const list = ref(new Array(10).fill(0).map(() => ({ key: String(idx++) })))
 
   let seq = true
+
   const makeRandomData = () => {
     seq = !seq
     list.value = list.value.sort((a, b) =>
@@ -38,7 +21,72 @@ const Counter = () => {
     return <span>{item.key}</span>
   })
 
+  function add() {
+    list.value.push({
+      key: String(idx++),
+    })
+  }
+
+  function remove() {
+    const _idx = ~~(Math.random() * list.value.length)
+
+    const [removed] = list.value.splice(_idx, 1)
+
+    console.log('removed', toRaw(removed))
+  }
+
+  return (
+    <>
+      <h1>Test loop</h1>
+      <button onClick={makeRandomData}>random</button>
+      <button onClick={add}>add</button>
+      <button onClick={remove}>remove</button>
+      {testVMap}
+    </>
+  )
+}
+
+const TestIf = () => {
+  const show = ref(false)
+  const testVCase = vCase(show, {
+    true: () => <div>this is true</div>,
+    false: () => <div>this is false</div>,
+  })
+
+  const toggleShow = () => {
+    show.value = !show.value
+  }
+
+  return (
+    <>
+      <h1>Test condition</h1>
+      <button onClick={toggleShow}>toggle</button>
+      {show}
+
+      {testVCase}
+    </>
+  )
+}
+
+const TestBinding = () => {
   const inputValue = ref('123')
+
+  return (
+    <>
+      <h1>Value binding</h1>
+      <input $value:trim={inputValue}></input>
+      <span>input value is: {inputValue}</span>
+    </>
+  )
+}
+
+const App = () => {
+  const count = ref(0)
+
+  const click = () => {
+    count.value++
+    console.log('click', count.value)
+  }
 
   return (
     <>
@@ -47,36 +95,16 @@ const Counter = () => {
         double:
         {computed(() => count.value * 2)}
       </div>
+
       <hr />
+      <TestIf></TestIf>
+      <TestFor></TestFor>
 
-      <h1>Test condition</h1>
-      <button onClick={toggleShow}>toggle</button>
-      {show}
-
-      {testVCase}
-
-      <h1>Test loop</h1>
-      <button onClick={makeRandomData}>random</button>
-      {testVMap}
-      <h1>Value binding</h1>
-      <input $value:trim={inputValue}></input>
-      <span>input value is: {inputValue}</span>
+      <TestBinding></TestBinding>
     </>
   )
 }
 
-const root = <Counter></Counter>
+const root = <App></App>
 
-mount(root as any)
-
-function mount(dom: DComponent) {
-  const container = document.getElementById('app')!
-
-  if (isFragment(dom)) {
-    dom.moveTo(container)
-  } else {
-    container.append(dom)
-  }
-
-  getContext(dom).emit('mounted')
-}
+mount(root as any, '#app')
