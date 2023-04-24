31d1b1a3e2ce80a317db239e92883800b27cc89a
Jerry Wang <x.jerry.wang@gmail.com>
2023-04-24 09:14:50 +0800

    jsx inject

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
 import inspect from 'vite-plugin-inspect'
 import uno from 'unocss/vite'
+import path from 'path'
 
 export default defineConfig({
   esbuild: {
     jsx: 'transform',
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
+    jsxInject: `import { h, Fragment } from '@/core'`,
   },
   plugins: [inspect(), uno()],
+  resolve: {
+    alias: {
+      '@/': path.resolve('src') + '/',
+    },
+  },
 })
